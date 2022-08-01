3a05cd2b3007c63b8026408cc2458469b415e8fd
mh <nmh82766321@gmail.com>
2022-08-01 17:44:23 +0900

    feat: FlexLayout 추가

---
+++ MinFit/Sources/Library/Cell/LibraryCell.swift
@@ -0,0 +1,58 @@
+//
+//  LibraryCell.swift
+//  MinFit
+//
+//  Created by 노민희 on 2022/05/30.
+//
+
+import UIKit
+
+import FlexLayout
+import PinLayout
+
+class LibraryCell: UITableViewCell {
+    // MARK: UI
+    
+    let rootFlexContainer: UIView = UIView()
+    let exerciseImageView = UIImageView()
+    let titleLabel = UILabel().then {
+            $0.textColor = .black
+            $0.font = UIFont.systemFont(ofSize: 13, weight: .regular)
+        }
+    
+    // MARK: Initializing
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+      super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setupConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: Layout
+    
+    func setupConstraints() {
+        self.rootFlexContainer.flex
+            .direction(.row)
+            .justifyContent(.start)
+            .alignItems(.start)
+            .define { flex in
+                flex.addItem(self.exerciseImageView)
+                    .marginTop(10)
+                    .marginLeft(20)
+                    .marginBottom(10)
+                    .aspectRatio(of: self.exerciseImageView)
+                flex.addItem(self.titleLabel)
+                    .marginLeft(10)
+            }
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        self.rootFlexContainer.pin.all()
+        self.rootFlexContainer.flex.layout()
+    }
+}
--- MinFit/Sources/Library/LibraryViewController.swift
+++ MinFit/Sources/Library/LibraryViewController.swift
@@ -7,12 +7,30 @@
 
 import UIKit
 
+import FlexLayout
 import ReactorKit
 import RxCocoa
 import RxSwift
+import Then
+import PinLayout
 
 class LibraryViewController: UIViewController, View {
+    // MARK: Properties
+    
     var disposeBag = DisposeBag()
+    
+    // MARK: UI
+    
+    let rootFlexContainer: UIView = UIView()
+    let tableView = UITableView(
+        frame: .zero,
+        style: .plain
+    ).then {
+        $0.backgroundColor = .white
+        $0.register(LibraryCell.self, forCellReuseIdentifier: "LibraryCell")
+    }
+    
+    // MARK: Initializing
     
     init(reactor: LibraryViewReactor)  {
         super.init(nibName: nil, bundle: nil)
@@ -22,6 +40,32 @@ class LibraryViewController: UIViewController, View {
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+    
+    // MARK: Life Cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupConstraints()
+    }
+    
+    // MARK: Layout
+    
+    func setupConstraints() {
+        self.view.addSubview(self.rootFlexContainer)
+        
+        self.rootFlexContainer.flex.define { flex in
+            flex.addItem(self.tableView)
+        }
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        self.rootFlexContainer.pin.all(self.view.pin.safeArea)
+        self.rootFlexContainer.flex.layout()
+    }
+    
+    // MARK: bind
     
     func bind(reactor: LibraryViewReactor) {
         reactor.action.onNext(Reactor.Action.refresh)
